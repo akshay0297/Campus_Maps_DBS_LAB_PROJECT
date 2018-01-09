29b79674ebfcef6583721c4f463b343e6d808e69
akshay0297 <akshay0297@gmail.com>
2018-01-09 22:37:26 +0530

    Main commit #1

---
+++ DBSLABPROJ/Form4.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace DBSLABPROJ
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dest = new System.Windows.Forms.ComboBox();
+            this.source = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.outp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(14, 11, 14, 11);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.outp);
+            this.splitContainer1.Panel1.Controls.Add(this.dest);
+            this.splitContainer1.Panel1.Controls.Add(this.source);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.l1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.geckoWebBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1262, 628);
+            this.splitContainer1.SplitterDistance = 420;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dest
+            // 
+            this.dest.FormattingEnabled = true;
+            this.dest.Items.AddRange(new object[] {
+            "313",
+            "AB1",
+            "AB1BG",
+            "AB2",
+            "AB5BG",
+            "AB5MainEnterance",
+            "Cafeteria",
+            "ChemBlock",
+            "CS",
+            "FC",
+            "IC",
+            "ICTDept",
+            "Lab1",
+            "Lab8",
+            "Library",
+            "NLH"});
+            this.dest.Location = new System.Drawing.Point(165, 188);
+            this.dest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dest.Name = "dest";
+            this.dest.Size = new System.Drawing.Size(180, 24);
+            this.dest.TabIndex = 5;
+            // 
+            // source
+            // 
+            this.source.FormattingEnabled = true;
+            this.source.Items.AddRange(new object[] {
+            "313",
+            "AB1",
+            "AB1BG",
+            "AB2",
+            "AB5BG",
+            "AB5MainEnterance",
+            "Cafeteria",
+            "ChemBlock",
+            "CS",
+            "FC",
+            "IC",
+            "ICTDept",
+            "Lab1",
+            "Lab8",
+            "Library",
+            "NLH"});
+            this.source.Location = new System.Drawing.Point(165, 107);
+            this.source.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(180, 24);
+            this.source.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(122, 270);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Map IT !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 194);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destinaton";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source";
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.l1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.l1.Location = new System.Drawing.Point(66, 42);
+            this.l1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(0, 20);
+            this.l1.TabIndex = 0;
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.geckoWebBrowser1.Margin = new System.Windows.Forms.Padding(14, 11, 14, 11);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(832, 624);
+            this.geckoWebBrowser1.TabIndex = 1;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            this.geckoWebBrowser1.Click += new System.EventHandler(this.geckoWebBrowser1_Click);
+            // 
+            // outp
+            // 
+            this.outp.AutoSize = true;
+            this.outp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outp.Location = new System.Drawing.Point(18, 411);
+            this.outp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outp.Name = "outp";
+            this.outp.Size = new System.Drawing.Size(0, 20);
+            this.outp.TabIndex = 6;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1262, 628);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form4";
+            this.Text = "Map Page";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.ComboBox dest;
+        private System.Windows.Forms.ComboBox source;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.Label outp;
+    }
+}
